dcdd897bc0de0de788a50b3c4c9475554c8a7b35
Wade Morris <anunnakix@icloud.com>
2019-12-17 07:55:39 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
 def greeting(name)
-  puts "Hi! I'm HAL, what's your name?\nHello #{name}. It;s nice to meet you."
+  puts "Hi! I'm HAL, what's your name?Hello #{name}. It;s nice to meet you."
 end
